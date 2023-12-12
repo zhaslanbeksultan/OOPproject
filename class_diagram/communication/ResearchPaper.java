9aa4da8809dc867514702d106c3cbe215bfa4058
enaassk <114976417+enaassk@users.noreply.github.com>
2023-12-12 15:47:11 +0600

    ResearchPaper.java

--- class_diagram/communication/ResearchPaper.java
+++ class_diagram/communication/ResearchPaper.java
@@ -2,177 +2,88 @@ package communication;
 
 import Date;
 import Vector;
-
-/**
-* @generated
-*/
 public class ResearchPaper {
-    
-    /**
-    * @generated
-    */
+
     private int paperId;
-    
-    /**
-    * @generated
-    */
+  
     private String reasearchPaperTitle;
-    
-    /**
-    * @generated
-    */
+ 
     private String reasearchPaperWording;
-    
-    /**
-    * @generated
-    */
+ 
     private String researchPaperTopic;
-    
-    /**
-    * @generated
-    */
+ 
     private Vector researchPaperAuthors;
-    
-    /**
-    * @generated
-    */
+
     private Date reserchPaperWrittenDate;
     
-    
-    
-
-    /**
-    * @generated
-    */
     private int getPaperId() {
         return this.paperId;
     }
     
-    /**
-    * @generated
-    */
     private int setPaperId(Integer paperId) {
         this.paperId = paperId;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public String getReasearchPaperTitle() {
         return this.reasearchPaperTitle;
     }
-    
-    /**
-    * @generated
-    */
+
     public String setReasearchPaperTitle(String reasearchPaperTitle) {
         this.reasearchPaperTitle = reasearchPaperTitle;
     }
     
-    
-    /**
-    * @generated
-    */
     private String getReasearchPaperWording() {
         return this.reasearchPaperWording;
     }
-    
-    /**
-    * @generated
-    */
+
     private String setReasearchPaperWording(String reasearchPaperWording) {
         this.reasearchPaperWording = reasearchPaperWording;
     }
     
-    
-    /**
-    * @generated
-    */
     private String getResearchPaperTopic() {
         return this.researchPaperTopic;
     }
-    
-    /**
-    * @generated
-    */
+
     private String setResearchPaperTopic(String researchPaperTopic) {
         this.researchPaperTopic = researchPaperTopic;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private Vector getResearchPaperAuthors() {
         return this.researchPaperAuthors;
     }
-    
-    /**
-    * @generated
-    */
+
     private Vector setResearchPaperAuthors(Vector researchPaperAuthors) {
         this.researchPaperAuthors = researchPaperAuthors;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private Date getReserchPaperWrittenDate() {
         return this.reserchPaperWrittenDate;
     }
-    
-    /**
-    * @generated
-    */
+
     private Date setReserchPaperWrittenDate(Date reserchPaperWrittenDate) {
         this.reserchPaperWrittenDate = reserchPaperWrittenDate;
     }
-    
-    
-    
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public int getPaperId() {
-        //TODO
         return 0;
     }
-    
-    /**
-    * @generated
-    */
+
     public String getResearchPaperTopic() {
-        //TODO
+    
         return "";
     }
-    
-    /**
-    * @generated
-    */
+ 
     public Vector getResearchPaperAuthors() {
-        //TODO
+    
         return null;
     }
-    
-    /**
-    * @generated
-    */
+
     public Date getResearchPaperWrittenDate() {
-        //TODO
         return new Date();
     }
-    
-    /**
-    * @generated
-    */
+ 
     public int getArticleLength() {
-        //TODO
+    
         return 0;
     }
     
