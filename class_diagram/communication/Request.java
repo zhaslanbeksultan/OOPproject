06a435ee46a093222eae31ab18c0cd3d7006a520
enaassk <114976417+enaassk@users.noreply.github.com>
2023-12-12 15:45:34 +0600

    Request.java

--- class_diagram/communication/Request.java
+++ class_diagram/communication/Request.java
@@ -4,81 +4,45 @@ import users.Student;
 
 package communication;
 
-
-/**
-* @generated
-*/
 public class Request {
-    
-    /**
-    * @generated
-    */
+
     private Vector requests;
     
-    
-    /**
-    * @generated
-    */
     private User user;
-    
-    
 
-    /**
-    * @generated
-    */
     public Vector getRequests() {
         return this.requests;
     }
     
-    /**
-    * @generated
-    */
     public Vector setRequests(Vector requests) {
         this.requests = requests;
     }
     
-    
-    
-    /**
-    * @generated
-    */
+
     public User getUser() {
         return this.user;
     }
     
-    /**
-    * @generated
-    */
+  
     public User setUser(User user) {
         this.user = user;
     }
     
     
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public HashMap<Student,String> getRequest() {
-        //TODO
+       
         return null;
     }
     
-    /**
-    * @generated
-    */
     public HashMap<Employee,String> getRequest() {
-        //TODO
+        
         return null;
     }
     
-    /**
-    * @generated
-    */
+  
     public boolean processRequest() {
-        //TODO
+        
         return false;
     }
     
