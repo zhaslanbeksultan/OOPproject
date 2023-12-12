9156ff7f27a9b37d19ef263802efd80716881ab1
enaassk <114976417+enaassk@users.noreply.github.com>
2023-12-12 15:49:56 +0600

    ResearchProject.java

--- class_diagram/communication/ResearchProject.java
+++ class_diagram/communication/ResearchProject.java
@@ -3,245 +3,134 @@ import Vector;
 
 package communication;
 
-
-/**
-* @generated
-*/
 public class ResearchProject {
     
-    /**
-    * @generated
-    */
     private String topic;
-    
-    /**
-    * @generated
-    */
-    private Vector participants;
-    
-    /**
-    * @generated
-    */
-    private Vector publishedPapers;
-    
-    /**
-    * @generated
-    */
-    private Date publishedDate;
-    
-    /**
-    * @generated
-    */
-    private  supervisor;
-    
-    /**
-    * @generated
-    */
-    private  ;
-    
-    /**
-    * @generated
-    */
-    private Vector researchPapers;
-    
-    /**
-    * @generated
-    */
-    private String reviewCritique;
-    
-    
-    /**
-    * @generated
-    */
-    private Researcher researcher;
-    
-    
 
-    /**
-    * @generated
-    */
+    private Vector participants;
+
+    private Vector publishedPapers;
+
+    private Date publishedDate;
+
+    private  supervisor;
+
+    private  ;
+ 
+    private Vector researchPapers;
+
+    private String reviewCritique;
+ 
+    private Researcher researcher;
+
     private String getTopic() {
         return this.topic;
     }
-    
-    /**
-    * @generated
-    */
+ 
     private String setTopic(String topic) {
         this.topic = topic;
     }
-    
-    
-    /**
-    * @generated
-    */
+ 
     private Vector getParticipants() {
         return this.participants;
     }
     
-    /**
-    * @generated
-    */
+  
     private Vector setParticipants(Vector participants) {
         this.participants = participants;
     }
     
-    
-    /**
-    * @generated
-    */
+ 
     private Vector getPublishedPapers() {
         return this.publishedPapers;
     }
-    
-    /**
-    * @generated
-    */
+  
     private Vector setPublishedPapers(Vector publishedPapers) {
         this.publishedPapers = publishedPapers;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private Date getPublishedDate() {
         return this.publishedDate;
     }
-    
-    /**
-    * @generated
-    */
+  
     private Date setPublishedDate(Date publishedDate) {
         this.publishedDate = publishedDate;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private  getSupervisor() {
         return this.supervisor;
     }
-    
-    /**
-    * @generated
-    */
+
     private  setSupervisor(invalid supervisor) {
         this.supervisor = supervisor;
     }
     
-    
-    /**
-    * @generated
-    */
+
     public  get() {
         return this.;
     }
-    
-    /**
-    * @generated
-    */
+  
     public  set(invalid ) {
         this. = ;
     }
     
-    
-    /**
-    * @generated
-    */
+
     public Vector getResearchPapers() {
         return this.researchPapers;
     }
     
-    /**
-    * @generated
-    */
+ 
     public Vector setResearchPapers(Vector researchPapers) {
         this.researchPapers = researchPapers;
     }
     
-    
-    /**
-    * @generated
-    */
+
     private String getReviewCritique() {
         return this.reviewCritique;
     }
-    
-    /**
-    * @generated
-    */
+
     private String setReviewCritique(String reviewCritique) {
         this.reviewCritique = reviewCritique;
     }
     
-    
-    
-    /**
-    * @generated
-    */
+
     public Researcher getResearcher() {
         return this.researcher;
     }
-    
-    /**
-    * @generated
-    */
+   
     public Researcher setResearcher(Researcher researcher) {
         this.researcher = researcher;
     }
+
+    public String getResearchProjectTopic() {
+       
+        return "";
+    }
+
+    public Vector getWritersOfResearch() {
+     
+        return null;
+    }
+
+    public String getSupervisor() {
+        
+        return "";
+    }
     
-    
+  
+    public ResearchPaper toReferenceToPapers() {
+  
+        return null;
+    }
     
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public String getResearchProjectTopic() {
-        //TODO
-        return "";
-    }
-    
-    /**
-    * @generated
-    */
-    public Vector getWritersOfResearch() {
-        //TODO
-        return null;
-    }
-    
-    /**
-    * @generated
-    */
-    public String getSupervisor() {
-        //TODO
-        return "";
-    }
-    
-    /**
-    * @generated
-    */
-    public ResearchPaper toReferenceToPapers() {
-        //TODO
-        return null;
-    }
-    
-    /**
-    * @generated
-    */
     public String getReview() {
-        //TODO
+     
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public boolean checkSupervisorEligibility() {
-        //TODO
+
         return false;
     }
     
