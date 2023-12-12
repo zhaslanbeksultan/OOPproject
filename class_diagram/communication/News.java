8445c8a73b6e6c1bab254560ed549f0b65ee8ff5
enaassk <114976417+enaassk@users.noreply.github.com>
2023-12-12 15:43:52 +0600

    News.java

--- class_diagram/communication/News.java
+++ class_diagram/communication/News.java
@@ -2,110 +2,65 @@ package communication;
 
 import Vector;
 
-/**
-* @generated
-*/
 public class News {
-    
-    /**
-    * @generated
-    */
+
     private NewsType newsType;
     
-    /**
-    * @generated
-    */
     private String newsTitle;
     
-    /**
-    * @generated
-    */
+
     private String newsWording;
-    
-    /**
-    * @generated
-    */
+   
     private Vector newsComments;
     
-    
-    /**
-    * @generated
-    */
-    private User user;
-    
-    
 
-    /**
-    * @generated
-    */
+    private User user;
+
     private NewsType getNewsType() {
         return this.newsType;
     }
     
-    /**
-    * @generated
-    */
+   
     private NewsType setNewsType(NewsType newsType) {
         this.newsType = newsType;
     }
     
     
-    /**
-    * @generated
-    */
+
     private String getNewsTitle() {
         return this.newsTitle;
     }
     
-    /**
-    * @generated
-    */
+
     private String setNewsTitle(String newsTitle) {
         this.newsTitle = newsTitle;
     }
     
-    
-    /**
-    * @generated
-    */
+ 
     private String getNewsWording() {
         return this.newsWording;
     }
-    
-    /**
-    * @generated
-    */
+ 
     private String setNewsWording(String newsWording) {
         this.newsWording = newsWording;
     }
     
-    
-    /**
-    * @generated
-    */
+  
     private Vector getNewsComments() {
         return this.newsComments;
     }
     
-    /**
-    * @generated
-    */
+
     private Vector setNewsComments(Vector newsComments) {
         this.newsComments = newsComments;
     }
     
     
-    
-    /**
-    * @generated
-    */
+
     public User getUser() {
         return this.user;
     }
-    
-    /**
-    * @generated
-    */
+  
     public User setUser(User user) {
         this.user = user;
     }
@@ -113,37 +68,26 @@ public class News {
     
     
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public boolean viewNews() {
-        //TODO
+    
         return false;
     }
     
-    /**
-    * @generated
-    */
+  
     public String getNewsTitle() {
-        //TODO
+        
         return "";
     }
     
-    /**
-    * @generated
-    */
+  
     public String respondToNews() {
-        //TODO
+        
         return "";
     }
     
-    /**
-    * @generated
-    */
+ 
     public String getNewsWording() {
-        //TODO
+        
         return "";
     }
     
