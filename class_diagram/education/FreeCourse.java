7576e4404afe3799666d4caeaaec6e8ad0d201c6
Beksultan <64122603+zhaslanbeksultan@users.noreply.github.com>
2023-12-19 20:59:24 +0600

    s -

--- class_diagram/education/FreeCourse.java
+++ class_diagram/education/FreeCourse.java
@@ -6,4 +6,11 @@ public class FreeCourse extends Course {
     public FreeCourse(String courseId, String courseName, int numberOfCredits, DisciplineType disciplineType, String specialtyId) {
         super(courseId, courseName, numberOfCredits, disciplineType, specialtyId);
     }
+
+	@Override
+	public void addDropDiscipline() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
